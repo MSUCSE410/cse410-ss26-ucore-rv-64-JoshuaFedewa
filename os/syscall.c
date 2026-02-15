eec48fae0e7c32134a07f93293c2ab11686a1e50
JoshuaFedewa <143362237+JoshuaFedewa@users.noreply.github.com>
2026-02-15 23:37:20 +0000

    Chapter 3 Submission pt 2

--- os/syscall.c
+++ os/syscall.c
@@ -39,6 +39,18 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 /*
 * LAB1: you may need to define sys_task_info here
 */
+int sys_task_info(TaskInfo *ti) {
+	struct proc *p = curr_proc();
+
+	for(int i = 0; i < MAX_SYSCALL_NUM; i++) {
+		ti->syscall_times[i] = p->syscall_times[i];
+	}
+
+	ti->status = Running;
+	ti->time = (get_cycle() - p->start_time) / (CPU_FREQ / 1000);
+
+	return 0;
+}
 
 extern char trap_page[];
 
@@ -53,6 +65,14 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	struct proc *p = curr_proc();
+
+	if (id >= 0 && id < MAX_SYSCALL_NUM) {
+		p->syscall_times[id]++;
+	}
+
+	TaskInfo *ti;
+
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -69,6 +89,11 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	case SYS_task_info:
+		ti = (TaskInfo *)args[0];
+		ret = sys_task_info(ti);
+		break;
+
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
--- os/syscall.h
+++ os/syscall.h
@@ -1,6 +1,10 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include "proc.h"
+
 void syscall();
+
+int sys_task_info(TaskInfo *ti);
 
 #endif // SYSCALL_H
